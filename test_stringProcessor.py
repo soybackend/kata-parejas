af698843db3c3fdc343bd4eb6c1ad487dbf7d3fe
soybackend <josealvarez712@gmail.com>
2016-09-21 19:54:29 -0500

    test for string with one number failling

--- test_stringProcessor.py
+++ test_stringProcessor.py
@@ -4,3 +4,6 @@ from StringProcessor import StringProcessor
 class TestStringProcessor(TestCase):
     def test_process_string_empty(self):
         self.assertEqual(StringProcessor().process(""), [0], "String Empty")
+
+    def test_process_string_with_one_number(self):
+        self.assertEqual(StringProcessor().process("1"), [1], "One Number Sent")
