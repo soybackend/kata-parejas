7277e3ea87abc987dfaf8f95a09d8d36b11134df
soybackend <josealvarez712@gmail.com>
2016-09-21 20:48:02 -0500

    string with n numbers and min, max and avg test passed

--- StringProcessor.py
+++ StringProcessor.py
@@ -4,6 +4,6 @@ class StringProcessor:
             return [0, 0, 0, 0]
         elif "," in string:
             array = string.split(",")
-            return [len(array), int(min(array)), int(max(array)), float(sum(map(int, array)))/2]
+            return [len(array), int(min(array)), int(max(array)), float(sum(map(int, array)))/len(array)]
         else:
             return [1, int(string), int(string), int(string)]
