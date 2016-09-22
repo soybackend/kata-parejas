ba750e758c9e83be116a0deef20ba6b6ad578aa8
soybackend <josealvarez712@gmail.com>
2016-09-21 20:34:21 -0500

    empty string with min, max and avg test failed'

--- test_stringProcessor.py
+++ test_stringProcessor.py
@@ -3,7 +3,7 @@ from StringProcessor import StringProcessor
 
 class TestStringProcessor(TestCase):
     def test_process_string_empty(self):
-        self.assertEqual(StringProcessor().process(""), [0, 0, 0], "String Empty")
+        self.assertEqual(StringProcessor().process(""), [0, 0, 0, 0], "String Empty")
 
     def test_process_string_with_one_number(self):
         self.assertEqual(StringProcessor().process("1"), [1, 1, 1], "One Number Sent")
