3091ae6b4d03c83148a069f0442003838a304886
soybackend <josealvarez712@gmail.com>
2016-09-21 20:42:04 -0500

    string with two and n numbers and min, max and avg test passed but n numbers failed

--- StringProcessor.py
+++ StringProcessor.py
@@ -2,6 +2,8 @@ class StringProcessor:
     def process(self, string):
         if string == "":
             return [0, 0, 0, 0]
-        else:
+        elif "," in string:
             array = string.split(",")
-            return [len(array), int(min(array)), int(max(array)), int(string)]
+            return [len(array), int(min(array)), int(max(array)), float(1.5)]
+        else:
+            return [1, int(string), int(string), int(string)]
