148b386c595de012117300e34ec23182687ccee4
soybackend <josealvarez712@gmail.com>
2016-09-21 20:06:49 -0500

    test for empty string and min number passed

--- StringProcessor.py
+++ StringProcessor.py
@@ -1,7 +1,7 @@
 class StringProcessor:
     def process(self, string):
         if string == "":
-            return [0]
+            return [0, 0]
         else:
             array = string.split(",")
             return [len(array)]
