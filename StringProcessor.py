a3b2973b914205e2cc24f26a3d26c3484a159141
soybackend <josealvarez712@gmail.com>
2016-09-21 19:57:47 -0500

    test for string with two numbers passed

--- StringProcessor.py
+++ StringProcessor.py
@@ -2,5 +2,7 @@ class StringProcessor:
     def process(self, string):
         if string == "":
             return [0]
+        elif "," in string:
+            return [2]
         else:
             return [1]
