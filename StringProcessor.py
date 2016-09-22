59655681e360fd4abe21b69cfc46d9c6cbfbdc24
soybackend <josealvarez712@gmail.com>
2016-09-21 20:46:15 -0500

    string with two and n numbers and min, max and avg test passed but n numbers failed, refactored

--- StringProcessor.py
+++ StringProcessor.py
@@ -4,6 +4,6 @@ class StringProcessor:
             return [0, 0, 0, 0]
         elif "," in string:
             array = string.split(",")
-            return [len(array), int(min(array)), int(max(array)), float(1.5)]
+            return [len(array), int(min(array)), int(max(array)), float(sum(map(int, array)))/2]
         else:
             return [1, int(string), int(string), int(string)]
